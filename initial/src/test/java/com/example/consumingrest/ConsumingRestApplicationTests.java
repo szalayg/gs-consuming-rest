6f445b8c4340990807beae5dc7a12403afad5f69
szalayg <59998315+szalayg@users.noreply.github.com>
2022-08-30 11:44:41 +0200

    Update ConsumingRestApplicationTests.java
    updating to catfacts

--- initial/src/test/java/com/example/consumingrest/ConsumingRestApplicationTests.java
+++ initial/src/test/java/com/example/consumingrest/ConsumingRestApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.consumingrest;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
-class ConsumingRestApplicationTests {
+@SpringBootApplication
+public class ConsumingRestApplication {
 
-	@Test
-	void contextLoads() {
+	private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			CatFact catfact = restTemplate.getForObject(
+				"https://catfact.ninja/fact", CatFact.class);
+			log.info(catfact.toString());
+		};
+	}
 }
